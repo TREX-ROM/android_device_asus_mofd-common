52be2b3c21c79886d30868ca994fc9b46324ef93
LiquidDark <fisch890@gmail.com>
2016-06-21 21:24:41 +0200

    Update mofd.mk

--- mofd.mk
+++ mofd.mk
@@ -94,7 +94,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ADDITIONAL_DEFAULT_PROPERTIES += ro.dalvik.vm.native.bridge=libhoudini.so
 
 # Keyhandler
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     CMActions \
     com.cyanogenmod.keyhandler
 
